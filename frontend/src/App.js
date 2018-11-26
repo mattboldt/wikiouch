d185dbdd99e923625a95ba4780282182f46fcba2
Matt Boldt <me@mattboldt.com>
2018-11-26 00:09:35 -0600

    Added sidebar and random button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import queryString from 'query-string';
 import Article from './Article.js';
+import Sidebar from './Sidebar.js';
 import Api from './Api.js';
 
 class App extends Component {
@@ -25,20 +26,6 @@ class App extends Component {
     return <React.Fragment>
       <div id="header_outer" role="navigation">
         <div id="header">
-          <ul id="actions" role="menubar" aria-label="Header navigation">
-            <li id="nav_explore_li" className="nav_item" role="menuitem" aria-labelledby="nav_explore" style={{width: '175px', textAlign: 'center'}}>
-              <div className="nav_icon"></div>
-              <button
-                type="button"
-                className="nav"
-                id="nav_explore"
-                onClick={this.random}
-                style={{width: '100%', fontSize: '18px', border: 0, padding: '40px 0 0 0', background: 'none', cursor: 'pointer'}}>
-                RANDOM OUCH
-              </button>
-            </li>
-          </ul>
-
           <a onClick={this.random} style={{display :'block', paddingTop: '10px'}}>
             <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="wikiOuch logo" />
           </a>
@@ -49,8 +36,7 @@ class App extends Component {
       <div id="main_container">
         <div id="header_space"></div>
         <div id="main">
-          <div id="actionbar" className="" role="navigation">
-          </div>
+          <div id="actionbar" className="" role="navigation"></div>
 
           <div id="container" className="">
             <div id="article_shell">
@@ -59,6 +45,14 @@ class App extends Component {
                   <Article urls={this.urls} />
                 </div>
               </div>
+            </div>
+            <div id="sidebar">
+              <div className="sidebox">
+                <a onClick={this.random} className="search_button" style={{display: 'block', borderRadius: '5px'}}>
+                  RANDOM
+                </a>
+              </div>
+              {this.urls && <Sidebar urls={this.urls} />}
             </div>
           </div>
         </div>
---
+++ frontend/src/Sidebar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import queryString from 'query-string';
+
+class Sidebar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      a: props.urls[0],
+      b: props.urls[1],
+      c: props.urls[2]
+    }
+  }
+
+  onChange(prop, e) {
+    const value = e.target.value;
+    let obj = {}
+    obj[prop] = value;
+
+    this.setState(obj);
+  }
+
+  get url() {
+    const urls = Object.values(this.state).map((url) => {
+      return url.replace('//www', '//m');
+    });
+    return '/?' + queryString.stringify({ url: urls });
+  }
+
+  render() {
+    return <React.Fragment>
+      <div className="sidebox">
+        <h2>Create your own wikiOuch</h2>
+
+        {['a', 'b', 'c'].map((prop) => {
+          return <textarea
+            key={prop}
+            className="sidebar-input"
+            placeholder="wikiHow url"
+            value={this.state[prop]}
+            onChange={this.onChange.bind(this, prop)}></textarea>
+        })}
+        <a href={this.url} className="search_button" style={{display: 'block', borderRadius: '5px', marginTop: '10px'}}>
+          Oof Ouch Submit
+        </a>
+      </div>
+    </React.Fragment>
+  }
+}
+
+export default Sidebar
